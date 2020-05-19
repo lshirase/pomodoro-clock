1af4f431017d16a17b806dfd4249399f86a9ddf4
lshirase <lshirase@umich.edu>
2020-05-18 18:43:10 -0700

    set grid and decided on layout

---
+++ script.js
@@ -0,0 +1,17 @@
+//set global vars
+
+
+//select DOM items 
+const container = document.querySelector('#container');
+const skip = container.querySelector('#skip');
+const play = container.querySelector('#play');
+const set = container.querySelector('#set');
+const time = container.querySelector('#time');
+const action = container.querySelector('#action');
+const round = container.querySelector('#round');
+const goal = container.querySelector('#goal');
+
+//add event listeners
+clear.addEventListener('click',clearDisplay);
+
+//functions
